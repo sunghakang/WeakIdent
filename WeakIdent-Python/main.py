0e370e813719f66b2284f02100a2dee1a2e5de9e
mtang1001 <74107751+mtang1001@users.noreply.github.com>
2022-11-16 20:24:40 -0500

    Add error catching statements into main.py

--- WeakIdent-Python/main.py
+++ WeakIdent-Python/main.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
 import argparse
 import yaml
-# import numpy as np
 from utils import load_data, add_noise
 from model import weak_ident_pred
 from tabulate import tabulate
@@ -28,28 +25,87 @@ def main():
 
 
 def run():
-    global args
     args = parser.parse_args()
     with open(args.config) as f:
         config = yaml.safe_load(f)
-
     for key in config:
         for k, v in config[key].items():
             setattr(args, k, v)
 
-    print('Start loading arguments and dataset for', args.equation)
-    u, xs, true_coefficients = load_data('dataset-Python', args.filename)
-    u_hat = add_noise(u, args.sigma_SNR)
-    identification_err_table, eqn_table, coe_table, run_time = weak_ident_pred(
-        u_hat, xs, true_coefficients, args.max_dx, args.max_poly, args.stride_x,
-        args.stride_t, args.use_cross_der, args.Tau)
+    try:
+        print('Start loading arguments and dataset', args.filename, 'for', args.equation)
+    except AttributeError:
+        print('NameError occurred. The name of given data / equation is not defined in config file.') 
+        return
 
+    try: 
+        u, xs, true_coefficients = load_data('dataset-Python', args.filename)
+    except FileNotFoundError:
+        print('FileNotFoundError occurred. Dataset for ', args.equation, 'is missing')
+        return
+    
+    try: 
+        sigma_SNR = args.sigma_SNR
+    except AttributeError:
+        print('Noise signal ratio is missing. It is set to be 0.') 
+        sigma_SNR = 0
+    u_hat = add_noise(u, sigma_SNR)
+    
+    try:
+        use_cross_der = args.use_cross_der
+    except AttributeError:
+        use_cross_der = False
+        print('use_cross_der is set to be False.') 
+
+    try:
+        stride_x = args.stride_x
+    except AttributeError:
+        stride_x = 5
+        print('Set up a defalt stride in space in feature subsampling') 
+
+    try:
+        stride_t = args.stride_t
+    except AttributeError:
+        stride_t = 5
+        print('Set up a defalt stride in time in feature subsampling') 
+
+    try:
+        tau = args.Tau
+    except AttributeError:
+        tau = 0.05
+        print('Set up a defalt trimming score in support trimming') 
+
+    # set up default argument for dictionary building.    
+    try:
+        max_dx = args.max_dx
+    except AttributeError:
+        max_dx = 6
+    
+    try: 
+        assert max_dx <=8, "The feature library is suggested not to be too large. Reset max_dx = 6."
+    except AssertionError as msg:
+        print(msg)
+        max_dx = 6
+
+    try:
+        max_poly = args.max_poly
+    except AttributeError:
+        max_poly = 6
+
+    try:
+        identification_err_table, eqn_table, coe_table, run_time = weak_ident_pred(
+            u_hat, xs, true_coefficients, max_dx, max_poly, stride_x,
+            stride_t, use_cross_der, tau)
+    except AttributeError:
+        print('Additional necessary arguments are missing. Please make sure config file has all the arguments.')
+        return
+    
     print(
-        "\n ------------- coefficient vector overview ------noise-signal-ratio : %s  -------" % (args.sigma_SNR))
+        "\n ------------- coefficient vector overview ------noise-signal-ratio : %s  -------" % (sigma_SNR))
     print(tabulate(coe_table, headers= coe_table.columns, tablefmt="grid"))
     print(
         "\n ------------- equation overview ------noise-signal-ratio : %s  -------------------"
-        % (args.sigma_SNR))
+        % (sigma_SNR))
     print(tabulate(eqn_table, headers= eqn_table.columns, tablefmt="grid"))
     
     print("\n ------------------------------ CPU time: %s seconds ------------------------------" %
